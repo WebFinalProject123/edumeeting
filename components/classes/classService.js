06673d2cd9a75d9213b298e7669be4d7ef529a5a
Vodinhphuc1234 <64115737+Vodinhphuc1234@users.noreply.github.com>
2021-12-02 16:07:10 +0700

    fix image

--- components/classes/classService.js
+++ components/classes/classService.js
@@ -1,8 +1,9 @@
 
 const classModel= require('../../models/classModel')
 const Course = require('../../models/courseModel')
+const Schedule = require('../../models/scheduleModel')
 const teacher= require('../../models/teacherModel')
 const User = require('../../models/userModel')
 exports.list=(id)=> classModel.findAll({raw: true, where:{
     _course_ID: id
-}, include: [{model: Course, as: 'Course'},{ model: teacher, as: 'Teacher', include: {model: User, as: 'User', attributes: ['_firstName', '_lastName']}, attributes: ['_teacher_ID']}]})
+}, include: [{model: Course, as: 'Course'},{model: Schedule, as: 'Schedule1'},{model: Schedule, as: 'Schedule2'},{ model: teacher, as: 'Teacher', include: {model: User, as: 'User', attributes: ['_firstName', '_lastName']}, attributes: ['_teacher_ID']}]})
